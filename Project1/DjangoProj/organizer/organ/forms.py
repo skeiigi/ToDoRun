adb1503dd500242166f89cf402e10ebbae2d4ad5
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-03-20 03:22:33 +0700

    update(auth_register.html): refors register

--- Project1/DjangoProj/organizer/organ/forms.py
+++ Project1/DjangoProj/organizer/organ/forms.py
@@ -39,6 +39,7 @@ class RegisterForm(UserCreationForm):
     email = forms.EmailField(
         help_text="Введите действительный email (например: user@example.com)"
     )
+
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
